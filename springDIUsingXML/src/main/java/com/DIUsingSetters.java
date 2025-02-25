4c0a710064ac015e5b04ae21bfcc627be9d32a66
Farrux Temurov <tfarrux70@gmail.com>
2025-02-25 22:43:34 +0500

    setter based DI example added

---
+++ springDIUsingXML/src/main/java/com/DIUsingSetters.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DIUsingSetters {
+
+	public static void main(String[] args) {
+		Resource res = new ClassPathResource("beans.xml");
+		// BeanFactory object created
+		BeanFactory factory = new XmlBeanFactory(res);
+		Employee employee1 = (Employee) factory.getBean("emp3"); // parameter constructor
+		employee1.displayEmployee();
+		Employee employee2 = (Employee) factory.getBean("emp4"); // it call empty constructor and using property tag
+																	// override the values
+
+		employee2.displayEmployee();
+
+	}
+
+}
