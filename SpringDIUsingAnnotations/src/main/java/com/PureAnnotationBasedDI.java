79716dbf07c25041f63f7d3810ccfb43561d6e77
Farrux Temurov <tfarrux70@gmail.com>
2025-02-28 23:13:27 +0500

    updated PureAnnotationBasedDI

--- SpringDIUsingAnnotations/src/main/java/com/PureAnnotationBasedDI.java
+++ SpringDIUsingAnnotations/src/main/java/com/PureAnnotationBasedDI.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class PureAnnotationBasedDI {
 
 	public static void main(String[] args) {
-		// it load configuration class.
+		// it loads configuration class.
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MyConfiguration.class);
 
 		Address add = (Address) ac.getBean("address");
 		System.out.println(add.toString());// default value of city and state
 		add.setCity("San Diego");
 		add.setState("California");
-		System.out.println(add); // by default it call toString method
+		System.out.println(add); // by default it calls toString method
 		Employee emp = (Employee) ac.getBean("employee");
 		System.out.println(emp);
 		emp.setId(100);
