7f5fc1b035b534a1d4f49b52071b48a50623d1af
Farrux Temurov <tfarrux70@gmail.com>
2025-03-01 16:13:46 +0500

    updated EmployeeDao

--- SpringWithDataSource/src/main/java/com/dao/EmployeeDao.java
+++ SpringWithDataSource/src/main/java/com/dao/EmployeeDao.java
@@ -21,7 +21,7 @@ public class EmployeeDao {
 	public int storeEmployee(Employee employee) {
 		try {
 			Connection con = ds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement("insert into employee values(?,?,?)");
+			PreparedStatement pstmt = con.prepareStatement("insert into employees values(?,?,?)");
 			pstmt.setInt(1, employee.getId());
 			pstmt.setString(2, employee.getName());
 			pstmt.setFloat(3, employee.getSalary());
@@ -32,4 +32,48 @@ public class EmployeeDao {
 		}
 	}
 
+	public int deleteEmployee(int id) {
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("delete from employees where id=?");
+			pstmt.setInt(1, id);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.err.println(e.toString());
+			return 0;
+		}
+	}
+
+	public int updateEmployee(Employee employee) {
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("update employees set salary=?,name=? where id=?");
+			pstmt.setFloat(1, employee.getSalary());
+			pstmt.setString(2, employee.getName());
+			pstmt.setInt(3, employee.getId());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.err.println(e.toString());
+			return 0;
+		}
+	}
+
+	public List<Employee> findAllEmployee() {
+		List<Employee> listofEmp = new ArrayList<Employee>();
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("select * from employees");
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Employee emp = new Employee();
+				emp.setId(rs.getInt(1));
+				emp.setName(rs.getString(2));
+				emp.setSalary(rs.getFloat(3));
+				listofEmp.add(emp);
+			}
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+		return listofEmp;
+	}
 }
