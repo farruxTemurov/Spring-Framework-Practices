07a1093f435a0f38b70a3bb75aa50d386b65bed8
Farrux Temurov <tfarrux70@gmail.com>
2025-03-02 11:36:35 +0500

    SpringJdbcTemplate setup ready

---
+++ SpringJdbcTemplate/src/main/java/com/dao/EmployeeDao.java
@@ -0,0 +1,79 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.bean.Employee;
+
+@Repository // DAO layer annotation
+public class EmployeeDao {
+
+	@Autowired
+	DataSource ds; // it search database connection in beans.xml file
+	// if present it do DI for that data source. by default it consider as byType.
+
+	public int storeEmployee(Employee employee) {
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("insert into employees values(?,?,?)");
+			pstmt.setInt(1, employee.getId());
+			pstmt.setString(2, employee.getName());
+			pstmt.setFloat(3, employee.getSalary());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.err.println(e.toString());
+			return 0;
+		}
+	}
+
+	public int deleteEmployee(int id) {
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("delete from employees where id=?");
+			pstmt.setInt(1, id);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.err.println(e.toString());
+			return 0;
+		}
+	}
+
+	public int updateEmployee(Employee employee) {
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("update employees set salary=?,name=? where id=?");
+			pstmt.setFloat(1, employee.getSalary());
+			pstmt.setString(2, employee.getName());
+			pstmt.setInt(3, employee.getId());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.err.println(e.toString());
+			return 0;
+		}
+	}
+
+	public List<Employee> findAllEmployees() {
+		List<Employee> listofEmp = new ArrayList<Employee>();
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("select * from employees");
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Employee emp = new Employee();
+				emp.setId(rs.getInt(1));
+				emp.setName(rs.getString(2));
+				emp.setSalary(rs.getFloat(3));
+				listofEmp.add(emp);
+			}
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+		return listofEmp;
+	}
+}
---
+++ SpringJdbcTemplate/src/main/java/com/service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bean.Employee;
+import com.dao.EmployeeDao;
+
+@Service // service layer annotation which contains business logic
+public class EmployeeService {
+
+	@Autowired
+	EmployeeDao employeeDao;
+
+	public String storeEmployee(Employee employee) {
+		if (employee.getSalary() < 10000) {
+			return "Salary must be > 10000";
+		} else if (employeeDao.storeEmployee(employee) > 0) {
+			return "Employee record stored successfully";
+		} else {
+			return "Employee record dint' store";
+		}
+	}
+
+	public String deleteEmployee(int id) {
+		int result = employeeDao.deleteEmployee(id); // Call DAO method
+		if (result > 0) {
+			return "Employee deleted successfully.";
+		} else {
+			return "Employee not found.";
+		}
+	}
+
+	public String updateEmployee(Employee employee) {
+		int result = employeeDao.updateEmployee(employee); // Call DAO method
+		if (result > 0) {
+			return "Employee updated successfully.";
+		} else {
+			return "Employee not found.";
+		}
+	}
+
+	public List<Employee> viewAllEmployees() {
+		return employeeDao.findAllEmployees(); // Fetch all employees from DAO
+	}
+
+}
