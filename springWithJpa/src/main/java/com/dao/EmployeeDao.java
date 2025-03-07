1b8b2047803e09d227bcf554fcee8914722cca51
Farrux Temurov <tfarrux70@gmail.com>
2025-03-07 21:42:37 +0500

    added delete and update operations in dao layer

--- springWithJpa/src/main/java/com/dao/EmployeeDao.java
+++ springWithJpa/src/main/java/com/dao/EmployeeDao.java
@@ -28,4 +28,36 @@ public class EmployeeDao {
 			return 0;
 		}
 	}
+
+	// Delete Employee using id
+	public int deleteEmployee(int id) {
+		EntityManager manager = emf.createEntityManager();
+		EntityTransaction tran = manager.getTransaction();
+		Employee emp = manager.find(Employee.class, id);// find record using pk property
+		if (emp == null) {
+			return 0;
+		} else {
+			tran.begin();
+			manager.remove(emp); // delete query
+			tran.commit();
+			return 1;
+		}
+	}
+
+	// update employee salary using id
+	public int updateEmployeeSalary(Employee employee) {
+		EntityManager manager = emf.createEntityManager();
+		EntityTransaction tran = manager.getTransaction();
+		Employee emp = manager.find(Employee.class, employee.getId());//
+		if (emp == null) {
+			return 0;
+		} else {
+			tran.begin();
+			emp.setSalary(employee.getSalary()); // replace new salary
+
+			manager.merge(emp); // update query
+			tran.commit();
+			return 1;
+		}
+	}
 }
