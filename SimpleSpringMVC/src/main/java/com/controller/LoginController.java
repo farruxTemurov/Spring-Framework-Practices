0b1aead1605b780045b17bb82c62fdf5f26fcda7
Farrux Temurov <tfarrux70@gmail.com>
2025-03-14 12:15:09 +0500

    LoginController added

---
+++ SimpleSpringMVC/src/main/java/com/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // controller specific annotation provide Servlet features
+public class LoginController {
+
+	// like a doPost method
+
+	@RequestMapping(value = "SignIn", method = RequestMethod.POST)
+	public ModelAndView loginSignIn(HttpServletRequest request) { // DI for request object
+		String emailid = request.getParameter("email");
+		String password = request.getParameter("password");
+		// check emailid and password
+		// we can check by hard coding or using jdbc or using jdbcTemplate or Jpa etc
+		ModelAndView mav = new ModelAndView();
+		if (emailid.equals("akash@gmail.com") && password.equals("123")) {
+			mav.addObject("user", "user@gmail.com"); // equal to request.setAttribute("user","user@gmail.com");
+			// request.setAttribute("user", "user@gmail.com");
+			mav.setViewName("success.jsp");
+		} else {
+			mav.setViewName("failure.jsp");
+		}
+
+		return mav;
+	}
+}
